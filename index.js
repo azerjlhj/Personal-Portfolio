38162346597409746a06f0eee90a11f5191c6883
Azer <azer.jalahej2@gmail.com>
2025-09-02 18:17:34 -0400

    Migrate portfolio (including all of what pertains to HTML5 code & Vanilla CSS) to Node.js + Express.js backend side.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '/public')))
+
 app.set('views', path.join(__dirname, "views"))
 app.set('view engine', 'ejs')
 
